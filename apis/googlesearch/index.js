5729090fb6ac74d6eba8804c01411ef99bf1bf6e
Ryan D. Watts <ryandwatts@gmail.com>
2017-10-20 16:09:44 -0700

    update .gitignore add env

--- apis/googlesearch/index.js
+++ apis/googlesearch/index.js
@@ -2,7 +2,7 @@ var request = require('request');
 var googleapis = require('googleapis');
 var qs = require('querystring');
 var apiurl = 'https://www.googleapis.com/webmasters/v3/sites/[SITE_URL]/searchAnalytics/query?key=';
-var apikey = '';
+var apikey = process.env.GOOGLE_API_KEY;
 
 console.log(googleapis);
 
