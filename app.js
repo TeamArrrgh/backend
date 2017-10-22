322fdac2c8617935541fdc6a98e4d88621d053dc
azjmk <jordan.kenned3@gmail.com>
2017-10-21 18:03:38 -0700

    final commit

--- app.js
+++ app.js
@@ -6,38 +6,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var redis = require('redis')
-
 var index = require('./routes/index');
-
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-
+var users = require('./routes/users');
 var googleSearchApi = require('./apis/googlesearch');
 var rawData = require('./apis/googlesearch/raw_webmaster_data.json');
 const googleTrends = require('google-trends-api');
-
-
-if(false) {
-  var privateKey  = fs.readFileSync('/etc/letsencrypt/live/simplex.ev.io/privkey.pem', 'utf8');
-  var certificate = fs.readFileSync('/etc/letsencrypt/live/simplex.ev.io/fullchain.pem', 'utf8');
-  
-  var credentials = {key: privateKey, cert: certificate};
-  
-  // your express configuration here
-} else {
-  var credentials = {key: "", cert: ""}
-}
-
-
+var moment = require('moment');
 
 var app = express();
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-
-
-
 
 //Redis shit.
 var client = redis.createClient('6379', 'simplex.ev.io');
@@ -54,37 +30,190 @@ var dataString = JSON.stringify(rawData);
 //   return res;
 // })
 
+// var arraySortBy = function(arr, prop, type, dir) {
+//     if (arr.length <= 1) { return arr; } // if the array is length 0 or 1 bail early
+    
+//     var isSingle = prop instanceof Array && prop[0] instanceof Array ? false : true;
+//     var sortOps;
+//     if (isSingle === true) {
+//       sortOps = [{ prop : prop, type : type, dir : dir }];
+//     } else {
+//       // if multiple the arguments come in on prop so we need to unfold to an object
+//       sortOps = new Array(prop.length);
+//       for(var i = 0; i < prop.length; i++) {
+//         sortOps[i] = { prop : prop[i][0], type : prop[i][1], dir : prop[i][2] };
+//       }
+//     }
+    
+//     // our prop should always be an array for lookup purposes
+//     for(var i = 0; i < sortOps.length; i++) {
+//       var op = sortOps[i];
+//       if (op.prop instanceof Array === false) { op.prop = [op.prop]; }
+//     }
+    
+//     // go through the array and extract the values from it so that way we don't have to do a look-up on each comparison
+//     var temp = new Array(arr.length);
+//     for(var i = 0; i < arr.length; i++) {
+//       var item = arr[i];
+//       var result = { values : new Array(sortOps.length), index : i, sortOps : sortOps };
+//       for(var j = 0; j < sortOps.length; j++) {
+//         var op = sortOps[j];
+        
+//         var value = item;
+//         for(var k = 0; k < op.prop.length; k++) {
+//           value = value[op.prop[k]];
+//           if (value === undefined) { break; }
+//         }
+        
+//         result.values[j] = {
+//           raw : value, // original value needed in some comparisons
+//           clean : op.type === "alpha" && value !== undefined ? value.toLowerCase() : value
+//         }
+//       }
+      
+//       temp[i] = result;
+//     }
+    
+//     temp.sort(propCompare);
+    
+//     // after the sort is completed, recompose our data array based on the indexes from our mapped array
+//     var done = new Array(temp.length);
+//     for(var i = 0; i < temp.length; i++) {
+//       done[i] = arr[temp[i].index];
+//     }
+//     return done;
+//   }
+// var propCompare = function(a, b) {
+//   for(var i = 0; i < a.sortOps.length; i++) {
+//     var op = a.sortOps[i];
+//     var aVal = a.values[i].clean;
+//     var aValRaw = a.values[i].raw;
+//     var bVal = b.values[i].clean;
+//     var bValRaw = b.values[i].raw;
+//     var comp;
+    
+//     if (aVal === undefined && bVal !== undefined) {
+//       comp = 1; // in an ascending sort undefined sorts to last, so if aVal is undefined bVal is smaller
+//     } else if (aVal !== undefined && bVal === undefined) {
+//       comp = -1; // in an ascending sort undefined sorts to last, so if bVal is undefined aVal is smaller
+//     } else if (aVal === undefined && bVal === undefined) {
+//       comp = 0; // both undefined, consider them equal
+//     } else if (op.type === "alpha") {
+//       // in alpha compare we want to compare without case first (all lower), and then with case, this way A goes before a and both go before B
+//       comp = aVal > bVal ? 1 : aVal < bVal ? -1 : aValRaw > bValRaw ? 1 : aValRaw < bValRaw ? -1 : 0;
+//     } else if (op.type === "numeric") {
+//       comp = aVal - bVal;
+//     } else if (op.type === "natural") {
+//       comp = naturalCompare(aVal, bVal);
+//     }
+    
+//     if (comp !== 0) {
+//       comp *= op.dir === "asc" ? 1 : -1; // with a descending query we reverse the polarity
+//       return comp;
+//     }
+//   }
+  
+//   return 0;
+// }
+
 // var reportStruct = {};
+// var count = 0;
+
 // rawData['rows'].forEach(function(row){
-//   console.log(row);
+//   var newDate = new moment(row.keys[1]);
+//   var dateKey = newDate.format('YYYY').toString() + '-' + newDate.format('MM').toString();
+//   var tempData = {};
+
+//   if (count === 0 || (reportStruct[dateKey] === undefined)) {
+//     reportStruct[dateKey] = {
+//       keywords : {},
+//       keywordsArray : []
+//     };
+//   }
+
+//   if (reportStruct[dateKey].keywords[row.keys[0]] === undefined){
+//     tempData = {
+//       keyword : row.keys[0],
+//       frequency : 1,
+//       clicks : row.clicks,
+//       impressions : row.impressions,
+//       ctr : (row.ctr * 100),
+//       position : row.position,
+//       weight : ((row.clicks + row.impressions) * row.ctr)
+//     };
+//     reportStruct[dateKey].keywords[row.keys[0]] = tempData;
+//     // reportStruct[dateKey].keywordsArray.push(tempData);
+//   } else if (reportStruct[dateKey].keywords[row.keys[0]] !== undefined) {
+//     reportStruct[dateKey].keywords[row.keys[0]].frequency++;
+//     reportStruct[dateKey].keywords[row.keys[0]].clicks += row.clicks;
+//     reportStruct[dateKey].keywords[row.keys[0]].impressions += row.impressions;
+//     reportStruct[dateKey].keywords[row.keys[0]].ctr += row.ctr;
+//     reportStruct[dateKey].keywords[row.keys[0]].position += row.position;
+//     reportStruct[dateKey].keywords[row.keys[0]].weight = (
+//       ((reportStruct[dateKey].keywords[row.keys[0]].clicks + reportStruct[dateKey].keywords[row.keys[0]].impressions) * reportStruct[dateKey].keywords[row.keys[0]].ctr) / 100
+//     );
+//   }
+//   count = count + 1;
+//   lastDateKey = dateKey;
 // });
 
+// for (var key in reportStruct) {
+//     // skip loop if the property is from prototype
+//     if (!reportStruct.hasOwnProperty(key)) continue;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//     var obj = reportStruct[key].keywords;
+//     for (var prop in obj) {
+//         // skip loop if the property is from prototype
+//         if(!obj.hasOwnProperty(prop)) continue;
+//         reportStruct[key].keywordsArray.push(obj[prop]);
+//     }
+//     reportStruct[key].keywordsArray = arraySortBy(reportStruct[key].keywordsArray, "weight", "numeric", "desc");
+// }
 
-app.set('view engine', 'jade');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// googleTrends.interestOverTime({keyword: 'laser quest', startTime: new Date('2017-05-01'), endTime: new Date('2017-10-31'), geo: 'US-WA-881'}).then((res) => {
+//   console.log('INTEREST OVER TIME: ');
+//   console.log(res);
+// }).catch((err) => {
+//   console.log(err);
+// })
+
+// googleTrends.relatedQueries({keyword: 'a finer moment spokane', startTime: new Date('2017-05-01'), endTime: new Date('2017-10-31'), geo: 'US-WA-881'})
+// .then((res) => {
+//   console.log('--------------------------------');
+//   console.log('RELATED QUERIES OVER TIME: ');
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// })
+
+var params = {keyword: 'laser quest', startTime: new Date('2017-05-01'), endTime: new Date('2017-10-31'), geo: 'US-WA-881'};
+
+var data = []
+googleSearchApi.get('www.visitspokane.com')['2017-10'].keywordsArray.slice(0, 10).forEach(function(elem) {
+  
+  params.keyword = elem.keyword;
+  var gtot = null;
+
+  googleTrends.interestOverTime(params).then((res) => {
+    // console.log(res);
+    gtot = res;
+    data.push({keyword: elem.keyword, frequency: elem.frequency, gtot})
+    console.log(data)
+  }).catch((err) => {
+    rtnData = [];
+  })
+
+  // googleSearchApi.gettrendsovertime(params);
+
+ // console.log(gtot);
 });
 
+// console.log(googleSearchApi.get('www.visitspokane.com')['2017-10'].keywordsArray);
 
-app.use('/*', index);
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+
 
 module.exports = app;
